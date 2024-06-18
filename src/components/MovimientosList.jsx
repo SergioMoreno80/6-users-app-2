890884eb16ef03ac69d043f192a348dfd4d24aca
Sergio Moreno <sergio.moreno@live.com.mx>
2024-06-17 18:28:58 -0600

    Version 3.3
    Actualizacion para poder agregar asignacion de activo, en caso que no exista ninguna asignacion.

--- src/components/MovimientosList.jsx
+++ src/components/MovimientosList.jsx
@@ -2,17 +2,7 @@ import { MovimientosRow } from "./MovimientosRow";
 import { useMovimientos } from "../hooks/useMovimientos";
 import { useAuth } from "../auth/hooks/useAuth";
 import React, { useState } from "react";
-import PropTypes from "prop-types";
-import Box from "@mui/material/Box";
-import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import MenuIcon from "@mui/icons-material/Menu";
-import AddIcon from "@mui/icons-material/Add";
 import { NavLink } from "react-router-dom"; // Importa NavLink
-import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
 import {
   Table,
   TableBody,
@@ -21,153 +11,95 @@ import {
   TableHead,
   TableRow,
   Paper,
-  Button,
   TextField, Fab
 } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
 export const MovimientosList = () => {
   const [isTableExpanded, setTableExpanded] = useState(true);
-
-  const toggleTableSize = () => {
-    setTableExpanded(!isTableExpanded);
-  };
   const { movimientos } = useMovimientos();
   const { login } = useAuth();
   const [searchTerm, setSearchTerm] = useState("");
   const searchColumns = ['descripcion', 'tipo_movimiento', 'id_departamento']; // Columnas en las que se realizará la búsqueda
 
   const handleSearchTermChange = (event) => {
-    console.log("", event.target.value);
     setSearchTerm(event.target.value);
   };
 
-
   const filteredMovimientos = movimientos.filter((mov) =>
-  searchColumns.some((column) =>
-    mov[column].toString().toLowerCase().includes(searchTerm.toLowerCase())
-  )
-);
+    searchColumns.some((column) =>
+      mov[column]?.toString().toLowerCase().includes(searchTerm.toLowerCase())
+    )
+  );
+
   return (
-
     <div style={{ margin: "20px", maxHeight: "700px", overflow: "auto" }}>
+      <TextField
+        label="Buscar movimiento"
+        variant="outlined"
+        value={searchTerm}
+        onChange={handleSearchTermChange}
+        fullWidth
+        margin="normal"
+        sx={{ maxWidth: "400px" }}
+      />
 
-    <TextField
-      label="Buscar movimiento"
-      variant="outlined"
-      value={searchTerm}
-      onChange={handleSearchTermChange}
-      fullWidth
-      margin="normal"
-      sx={{ maxWidth: "400px" }}
-    />
-     {/* Botón para descargar PDF */}
-     {/* <Fab
-          aria-label="download"
-          onClick={generatePDF}
-          style={{ 
-            marginLeft: "10px", 
-            marginTop: "25px",
-            backgroundColor: "#d32f2f", // Cambia el color aquí
-            color: "#fff" // Color del icono
-          }}
+      {/* Botón para abrir el formulario de registro */}
+      <NavLink to="/AssignAsset/process">
+        <Fab
+          color="primary"
+          aria-label="add"
+          style={{ marginLeft: "10px", marginTop: "25px" }}
         >
-          <PictureAsPdfIcon />
-        </Fab> */}
-        {/* Botón para abrir el formulario de registro */}
-        <NavLink to="/AssignAsset/process">
-          <Fab
-            color="primary"
-            aria-label="add"
-            style={{ marginLeft: "10px", marginTop: "25px" }}
-          >
-            <AddIcon />
-          </Fab>
-        </NavLink>
+          <AddIcon />
+        </Fab>
+      </NavLink>
 
-    <TableContainer
-      component={Paper}
-      style={{
-        marginLeft: isTableExpanded ? "40px" : "40",
-        marginTop: "10px",
-        transition: "margin-left 0.3s ease-in-out",
-        overflowX: "auto",
-      }}
-    >
-      <Table aria-label="collapsible table">
-        <TableHead style={{ backgroundColor: "#000" }}>
-          <TableRow>
-            <TableCell style={{ color: "#fff", fontWeight: "bold" }}>
-              ID
-            </TableCell>
-            <TableCell style={{ color: "#fff", fontWeight: "bold" }}
-            align="center">
-              TIPO DE MOVIMIENTO
-            </TableCell>
-            <TableCell
-              style={{ color: "#fff", fontWeight: "bold" }}
-              align="center"
-            >
-              FECHA DE MOVIMIENTO
-            </TableCell>
-            <TableCell
-              style={{ color: "#fff", fontWeight: "bold" }}
-              align="center"
-            >
-              DESCRIPCION
-            </TableCell>
-            <TableCell
-              style={{ color: "#fff", fontWeight: "bold" }}
-              align="center"
-            >
-              SUCURSAL
-            </TableCell>
-            <TableCell
-              style={{ color: "#fff", fontWeight: "bold" }}
-              align="center"
-            >
-              DEPARTAMENTO
-            </TableCell>
-            <TableCell
-              style={{ color: "#fff", fontWeight: "bold" }}
-              align="center"
-            >
-              PERSONAL
-            </TableCell>
-            <TableCell
-              style={{ color: "#fff", fontWeight: "bold" }}
-              align="center"
-            >
-              PDF
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        {/* <TableBody>
-          {
-                movimientos.map(({ //imagen, 
-                  id, tipo_movimiento, fecha_movimiento, descripcion, id_sucursal, id_departamento, empleado_id }) => (
-                    <MovimientosRow
-                        key={id}
-                        id={id}
-                        //imagen={imagen}
-                        tipo_movimiento={tipo_movimiento}
-                        fecha_movimiento={fecha_movimiento}
-                        descripcion={descripcion}
-                        id_sucursal={id_sucursal}
-                        id_departamento={id_departamento}
-                        empleado_id={empleado_id}
-                    />
-                ))
-            }
-           
-        </TableBody> */}
-        <TableBody>
+      <TableContainer
+        component={Paper}
+        style={{
+          marginLeft: isTableExpanded ? "40px" : "40",
+          marginTop: "10px",
+          transition: "margin-left 0.3s ease-in-out",
+          overflowX: "auto",
+        }}
+      >
+        <Table aria-label="collapsible table">
+          <TableHead style={{ backgroundColor: "#000" }}>
+            <TableRow>
+              <TableCell style={{ color: "#fff", fontWeight: "bold" }}>
+                ID
+              </TableCell>
+              <TableCell style={{ color: "#fff", fontWeight: "bold" }} align="center">
+                TIPO DE MOVIMIENTO
+              </TableCell>
+              <TableCell style={{ color: "#fff", fontWeight: "bold" }} align="center">
+                FECHA DE MOVIMIENTO
+              </TableCell>
+              <TableCell style={{ color: "#fff", fontWeight: "bold" }} align="center">
+                DESCRIPCION
+              </TableCell>
+              <TableCell style={{ color: "#fff", fontWeight: "bold" }} align="center">
+                SUCURSAL
+              </TableCell>
+              <TableCell style={{ color: "#fff", fontWeight: "bold" }} align="center">
+                DEPARTAMENTO
+              </TableCell>
+              <TableCell style={{ color: "#fff", fontWeight: "bold" }} align="center">
+                PERSONAL
+              </TableCell>
+              <TableCell style={{ color: "#fff", fontWeight: "bold" }} align="center">
+                PDF
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
             {filteredMovimientos.map((mov) => (
               <MovimientosRow key={mov.id} {...mov} />
             ))}
           </TableBody>
-      </Table>
-
-    </TableContainer>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
--- src/pages/MovimientosPage.jsx
+++ src/pages/MovimientosPage.jsx
@@ -3,7 +3,7 @@ import { UserModalForm } from "../components/UserModalForm";
 import { MovimientosList } from "../components/MovimientosList";
 import { useMovimientos } from "../hooks/useMovimientos";
 import { useAuth } from "../auth/hooks/useAuth";
-import { useParams } from "react-router-dom";
+import { useParams, NavLink } from "react-router-dom"; // Importa NavLink
 import { PaginatorA } from "../components/PaginatorA";
 
 export const MovimientosPage = () => {
@@ -14,49 +14,43 @@ export const MovimientosPage = () => {
     isLoading,
     paginator,
     handlerOpenForm,
-    //getMovimientos,
     getListMov,
   } = useMovimientos();
 
   const { login } = useAuth();
 
-  // useEffect(() => {
-  //   getMovimientos(page);
-  // }, [, page]);
   useEffect(() => {
-    // Elimina 'page' de los parámetros de la función getActivos
     getListMov();
   }, []);
 
   if (isLoading) {
     return (
       <div className="container my-4 text-center">
-        {/* <h4>Cargando ...</h4> */}
         <div className="spinner-border text-info" role="status">
           <span className="visually-hidden">Loading...</span>
         </div>
       </div>
     );
   }
+
   return (
     <>
       {!visibleForm || <UserModalForm />}
       <div className="container my-4 text-center">
         <h2>MOVIMIENTOS</h2>
-
         <div className="row">
-        <div className="col">
-
+          <div className="col">
             {movimientos.length === 0 ? (
               <div className="alert alert-warning">
                 No hay movimientos en el sistema!
+                <br />
+                <NavLink to="/AssignAsset/process" className="btn btn-primary mt-3">
+                  Agregar Asignación
+                </NavLink>
               </div>
             ) : (
               <>
                 <MovimientosList />
-                  {/* <PaginatorA url="/movimientos/page" paginator={paginator}
-                  />
-                */}
               </>
             )}
           </div>
